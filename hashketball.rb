016119b6c53e4000bf853c2707cfaa5beee1af13
Luis Torres <luistjr@gmail.com>
2020-10-20 14:30:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,7 +129,7 @@ end
 
 # hashketball #num_points_scored knows the number of points scored by each player
 
-def num_points_scored player_name 
+def num_points_scored(player_name)
 
   players = game_hash[:home][:players].merge(game_hash[:away][:players])
   binding.pry
