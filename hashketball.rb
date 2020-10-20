00d52d44f99f66857f55efd6b70f65f7405c399c
Luis Torres <luistjr@gmail.com>
2020-10-20 15:29:27 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,4 @@
-# Write your code below game_hash
+require 'pry'
 
 def game_hash
   {
@@ -127,4 +127,95 @@ def game_hash
   }
 end
 
-# Write code here
+def num_points_scored(player)
+  game_hash.each do |location, team_data|
+
+team_data[:players].each do |attribute|
+  
+  if attribute[:player_name] == player
+    return attribute[:points]
+  end 
+end 
+end 
+end
+
+def shoe_size(player)
+  game_hash.each do |location, team_data|
+
+team_data[:players].each do |attribute|
+  
+  if attribute[:player_name] == player
+    return attribute[:shoe]
+  end 
+end 
+end 
+end
+
+def team(team_name)
+  case team_name
+  when game_hash[:home][:team_name]
+    game_hash[:home]
+  when game_hash[:away][:team_name]
+    game_hash[:away]
+  end
+end 
+
+def team_colors(team_name)
+  team(team_name)[:colors]
+end 
+
+# * Build a method, `team_names`, that operates on the game `Hash` to return an
+#   `Array` of the team names.
+
+def team_names
+  [game_hash[:home][:team_name], game_hash[:away][:team_name]]
+end 
+
+# * Build a method, `player_numbers`, that takes in an argument of a team name and
+#   returns an `Array` of the jersey numbers for that team.
+
+def team_colors(team_name)
+  game_hash.each do |loc, team_info|
+    if team_info[:team_name] == team_name
+      return team_info[:colors]
+    end 
+  end 
+end 
+
+def player_numbers(team_name)
+  new_array = []
+  game_hash.each do |loc, team_info|
+   if team_info[:team_name] == team_name
+    team_info[:players].each do |player_info, stat|
+      new_array << player_info[:number]
+    end 
+   end 
+  end 
+  new_array
+end 
+
+def player_stats(players_name)
+  new_hash = {}
+  game_hash.each do |loc, team_info|
+    team_info[:players].each do |player_info, stat|
+      if player_info[:player_name] == players_name
+       new_hash = player_info
+     end 
+   end 
+ end 
+new_hash
+end 
+
+def big_shoe_rebounds
+  rebounds = 0
+  largest_shoe = 0
+  game_hash.each do |loc, team_info|
+    team_info[:players].each do |player_info, stat|
+      if player_info[:shoe] > largest_shoe
+       largest_shoe = player_info[:shoe]
+        rebounds = player_info[:rebounds] 
+      end 
+    end 
+  end 
+  rebounds
+end 
